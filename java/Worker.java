3361ef0e38c26f233b11d80de588643e97c7fb04
RadostinDonev <126570393+RadostinDonev@users.noreply.github.com>
2023-04-07 19:25:35 +0300

    Delete java directory

--- java/Worker.java
+++
@@ -1,8 +0,0 @@
-public abstract class Worker extends Thread {
-    private String name;
-    private int age;
-    public Worker(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-}
